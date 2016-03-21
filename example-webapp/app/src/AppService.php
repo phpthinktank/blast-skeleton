5236bbaff2d53125d3bb4b659730d72b92866e74
Marco Bunge <mab@grannyandsmith.com>
2016-03-21 14:51:51 +0100

    Add base app

---
+++ example-webapp/app/src/AppService.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ * (c) Marco Bunge <marco_bunge@web.de>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ *
+ * Date: 21.03.2016
+ * Time: 14:38
+ *
+ */
+
+namespace WebAppExample;
+
+
+class AppService
+{
+
+}
